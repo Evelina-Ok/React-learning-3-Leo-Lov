0ba0709f5335d75bde6c7ba823eb09bb43c938cf
Evelina-Ok <142876802+Evelina-Ok@users.noreply.github.com>
2024-08-21 15:00:40 +0200

    #3

--- src/components/Main/About/AboutContentTwo/AboutContentTwo.jsx
+++ src/components/Main/About/AboutContentTwo/AboutContentTwo.jsx
@@ -4,7 +4,7 @@ export const AboutContentTwo = () => {
     return (
         <>
          <figure className={style.aboutStyle}>
-            <img src="src/assets/images/building.png" alt="" />
+            
         <figcaption>
             <header>
             <h2>EVIG GARANTI</h2>
@@ -12,6 +12,7 @@ export const AboutContentTwo = () => {
                 <p>Leo-lov har altid været det bedste advokatfirma i verden. Der er simpelthen ingen der kan måle sig med os. Vi kan løse alle problemer så længe du har penge. Hvis du mod alt forventning skulle oplevel at støde på et retsligt problem som leo-lov ikke kan løse får du halvdelen af beløbet igen.</p>
                 <p>Det er sådan vi har skabt en forretning der ikke kan andet end at vokse!</p>
         </figcaption>
+        <img src="src/assets/images/building.png" alt="" />
         </figure>
         </>
     )
